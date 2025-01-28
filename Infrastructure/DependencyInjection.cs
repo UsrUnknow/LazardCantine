0f446ee23926c96e39a5991bbfe5d1941c6e77fd
UsrUnkn0w <maxfly779@gmail.com>
2025-01-28 22:40:44 +0100

    ➕feat: Configure dependency injection for services and repositories

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using LazardCantine.Services;
+
+namespace LazardCantine.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+    {
+        services.AddScoped<ClientRepository>();
+        services.AddScoped<ProduitRepository>();
+        return services;
+    }
+
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddScoped<ClientService>();
+        services.AddScoped<PlateauService>();
+        services.AddScoped<ProduitService>();
+        return services;
+    }
+}
